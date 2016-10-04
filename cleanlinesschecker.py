9a4a7f74c62ca61b4f0e75ba5d18d5a59fa8815d
elizabethshope <elizabeth.shope@gmail.com>
2016-10-04 14:50:20 -0400

    Update cleanlinesschecker.py
    The latest... Still have a bit more to go on this but we're getting there!

--- cleanlinesschecker.py
+++ cleanlinesschecker.py
@@ -20,30 +20,32 @@ def GasPriceUnclean():
         cleanliness.write("GAS PRICES\n")
     
     # Import the csv created in GasPrices() and count total records
-    gaspricedataframe = pd.read_csv("GasPrices.csv", sep = ',')
-    totalRecords = len(gaspricedataframe["Gas Price"])
+    dataframe = pd.read_csv("GasPrices.csv", sep = ',')
+    totalRecords = len(dataframe["Gas Price"])
     with open("data_cleanliness.txt", 'a') as cleanliness:
         cleanliness.write("Total gas price records: " + str(totalRecords) + "\n")
     
     # Count null records
-    nullRecords = gaspricedataframe["Gas Price"].isnull()
-    nullRecordsCount = len(gaspricedataframe[nullRecords])
+    nullRecords = dataframe["Gas Price"].isnull()
+    nullRecordsCount = len(dataframe[nullRecords])
     with open("data_cleanliness.txt", 'a') as cleanliness:
         cleanliness.write("Number of null records: " + str(nullRecordsCount) + "\n")
+        cleanliness.write("Fraction of missing values: " + str(nullRecordsCount/totalRecords) + "\n")
     
     # Count non-numeric values
-    numeric = gaspricedataframe.applymap(np.isreal)
+    numeric = dataframe.applymap(np.isreal)
     nonNumeric = numeric["Gas Price"] != True
     nonNumericCount = len(numeric[nonNumeric])
     with open("data_cleanliness.txt", 'a') as cleanliness:
-        cleanliness.write("Number of non-numeric values: " + str(nonNumericCount) + "\n")
+        cleanliness.write("Number of noise values: " + str(nonNumericCount) + "\n")
+        cleanliness.write("Fraction of noise values: " + str(nonNumericCount/totalRecords) + "\n")
     
     # Count values outside normal range
-    tooLow = gaspricedataframe["Gas Price"] <= 0.5
-    tooHigh = gaspricedataframe["Gas Price"] >= 5
+    tooLow = dataframe["Gas Price"] <= 0.5
+    tooHigh = dataframe["Gas Price"] >= 5
     outsideNormalCount = sum(tooLow) + sum(tooHigh)
     with open("data_cleanliness.txt", 'a') as cleanliness:
-        cleanliness.write("Number of records <= 0.5 or >= 5: " + str(outsideNormalCount) + "\n")
+        cleanliness.write("Number of records that don't make sense (too high or too low): " + str(outsideNormalCount) + "\n")
     
     # Sum total number of invalid records and get final score
     totalInvalid = nullRecordsCount + nonNumericCount + outsideNormalCount
@@ -59,6 +61,29 @@ def WeatherUnclean():
     # Prepare to write weather data
     with open("data_cleanliness.txt", 'a') as cleanliness:
         cleanliness.write("WEATHER DATA\n")
+        cleanliness.write("Note: only looking at avg temp, avg wind speed, and precipitation \n")
+        
+    # Import Weather.csv and count total records
+    dataframe = pd.read_csv("Weather.csv", sep = ',')
+    totalRecords = len(dataframe)
+    with open("data_cleanliness.txt", 'a') as cleanliness:
+        cleanliness.write("Total records: " + str(totalRecords) + "\n")
+    
+    # Define columns for which we want to check cleanliness
+    columns = ['Avg Temp', 'Avg Wind', 'Precip']
+    
+    for column in columns:
+        
+        # Count null records
+        nullRecords = dataframe[column].isnull()
+        nullRecordsCount = len(dataframe[nullRecords])
+        with open("data_cleanliness.txt", 'a') as cleanliness:
+            cleanliness.write("Number of null records for " + column + ": " + str(nullRecordsCount) + "\n")
+            cleanliness.write("Fraction of missing values for " + column + ": " + str(nullRecordsCount/totalRecords) + "\n")
+        
+        # Count non-numeric records
+        
+        # Count total number of invalid records per attribute and get final scores
     
 # Create function to check and describe Bikeshare station data (un)cleanliness
 def BikeshareUnclean():
